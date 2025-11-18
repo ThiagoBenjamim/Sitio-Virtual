95aed2d47da9912c2e5a630a30bb5ed7d6e5ac6f
Thiago <thiago08benjamim@gmail.com>
2025-11-18 03:27:25 -0300

    Adição da classe Funcionario e seus atributos

---
+++ Java/src/Principal/Funcionario.java
@@ -0,0 +1,33 @@
+package Principal;
+
+public class Funcionario {
+	private String nome;
+	private int ID;
+	private int CPF;
+	private double salario;
+	private String setor;
+	private String funcao;
+	private int tel[];
+	
+	public Funcionario(String nome, int ID, int CPF, double salario, String setor, String funcao, int tel[]) {
+		this.nome = nome;
+		this.ID = ID;
+		this.CPF = CPF;
+		this.salario = salario;
+		this.setor = setor;
+		this.funcao = funcao;
+		this.tel = tel;
+	}
+	
+	public void exibe() {
+		System.out.println("Nome:	" + nome);
+		System.out.println("ID:	" + ID);
+		System.out.println("CPF:	" + CPF);
+		System.out.println("Salario:	" + salario);
+		System.out.println("Setor:	" + setor);
+		System.out.println("Função:	" + funcao);
+		for (int i = 0; i < tel.length; i++) {
+			System.out.println("Telefone " + (i + 1) + ":	" + tel[i]);
+		}
+	}
+}
