19471c84ff4eb3be740ff3652ff91be47f580676
Thiago <thiago08benjamim@gmail.com>
2025-11-19 03:04:44 -0300

    Conclusão das classes Criadouro e Plantacao e seus contrutores
    Possíveis métodos futuros não inclusos

--- Java/src/Principal/Criadouro.java
+++ Java/src/Principal/Criadouro.java
@@ -1,5 +1,24 @@
 package Principal;
 
 public class Criadouro extends Setor {
-
+	private Animal animais[];
+	protected int indexAnimais;
+	
+	public Criadouro(int ID, double area, Funcionario funcionarios[], double custoLocal, Animal animais[]) {
+		super(ID, area, funcionarios, custoLocal);
+		this.animais = animais;
+		this.indexAnimais = 0;
+	}
+	
+	public void addAnimal(Animal animal) {
+		animais[indexAnimais] = animal;
+		indexAnimais += 1;
+	}
+	
+	public void exibeAnimais() {
+		for (int i = 0; i < animais.length; i++) {
+			System.out.print("Animal " + (i + 1) + ":	");
+			animais[i].exibe();
+		}
+	}
 }
--- Java/src/Principal/Plantacao.java
+++ Java/src/Principal/Plantacao.java
@@ -1,5 +1,24 @@
 package Principal;
 
 public class Plantacao  extends Setor {
-
+	private Cultivo cultivos[];
+	protected int indexCultivos;
+	
+	public Plantacao(int ID, double area, Funcionario funcionarios[], double custoLocal, Cultivo cultivos[]) {
+		super(ID, area, funcionarios, custoLocal);
+		this.cultivos = cultivos;
+		this.indexCultivos = 0;
+	}
+	
+	public void addCultivo(Cultivo cultivo) {
+		cultivos[indexCultivos] = cultivo;
+		indexCultivos += 1;
+	}
+	
+	public void exibeCultivos() {
+		for (int i = 0; i < cultivos.length; i++) {
+			System.out.print("Cultivo " + (i + 1) + ":	");
+			cultivos[i].exibe();
+		}
+	}
 }
