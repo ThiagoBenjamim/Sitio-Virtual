6b2a615c2ab75f96d976903bf2d8a1a1c3eb1f7b
Thiago <thiago08benjamim@gmail.com>
2025-11-19 03:03:44 -0300

    Conclusão das classes Animal e Cultivo e seus construtores
    Possíveis métodos futuros não inclusos

--- Java/src/Principal/Animal.java
+++ Java/src/Principal/Animal.java
@@ -1,5 +1,32 @@
 package Principal;
 
 public class Animal {
-
+	private String nome;
+	private int ID;
+	private String especie;
+	private String racao;
+	private double preco;
+	private double custoMensal;
+	
+	public Animal(String nome, int ID, String especie, String racao, double preco, double custoMensal) {
+		this.nome = nome;
+		this.ID = ID;
+		this.especie = especie;
+		this.racao = racao;
+		this.preco = preco;
+		this.custoMensal = custoMensal;
+	}
+	
+	public void exibe() {
+		System.out.println("Nome:	" + nome);
+		System.out.println("ID:	" + ID);
+		System.out.println("Especie:	" + especie);
+		System.out.println("Ração:	" + racao);
+		System.out.println("Preço do animal:	" + preco);
+		System.out.println("Custo mensal do animal:	" + custoMensal);
+	}
+	
+	public void exibeNome() {
+		System.out.println("Nome:	" + nome);
+	}
 }
--- Java/src/Principal/Cultivo.java
+++ Java/src/Principal/Cultivo.java
@@ -1,5 +1,29 @@
 package Principal;
 
 public class Cultivo {
-
+	private int ID;
+	private String especie;
+	private String tipoSolo;
+	private double preco;
+	private double custoMensal;
+	
+	public Cultivo(int ID, String especie, String tipoSolo, double preco, double custoMensal) {
+		this.ID = ID;
+		this.especie = especie;
+		this.tipoSolo = tipoSolo;
+		this.preco = preco;
+		this.custoMensal = custoMensal;
+	}
+	
+	public void exibe() {
+		System.out.println("ID:	" + ID);
+		System.out.println("Especie:	" + especie);
+		System.out.println("Tipo de solo do cultivo:	" + tipoSolo);
+		System.out.println("Preço do animal:	" + preco);
+		System.out.println("Custo mensal do animal:	" + custoMensal);
+	}
+	
+	public void exibeEspecie() {
+		System.out.println("Especie:	" + especie);
+	}
 }
