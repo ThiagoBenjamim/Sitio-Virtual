d562712e1cd8a0124ddd1312881b714a958a7303
Thiago <thiago08benjamim@gmail.com>
2025-11-19 03:02:09 -0300

    Conclusão da classe Setor e seu construtor
    Possíveis métodos futuros não inclusos

--- Java/src/Principal/Setor.java
+++ Java/src/Principal/Setor.java
@@ -1,5 +1,33 @@
 package Principal;
 
 public abstract class Setor {
+	private int ID;
+	private double area;
+	private Funcionario funcionarios[];
+	protected int indexFuncionarios;
+	private double custoLocal;
 	
+	public Setor(int ID, double area, Funcionario funcionarios[], double custoLocal) {
+		this.ID = ID;
+		this.area = area;
+		this.funcionarios = funcionarios;
+		this.custoLocal = custoLocal;
+		this.indexFuncionarios = 0;
+	}
+	
+	public void addFuncionario(Funcionario funcionario) {
+		funcionarios[indexFuncionarios] = funcionario;
+		indexFuncionarios += 1;
+	}
+	
+	public void exibe() {
+		System.out.println("ID:	" + ID);
+		System.out.println("Área do local:	" + area);
+		System.out.println("Custo do Local:	" + custoLocal);
+		if(funcionarios[0] != null) {
+			for (int i = 0; i < funcionarios.length; i++) {
+				funcionarios[i].exibeNome();
+			}
+		}
+	}
 }
